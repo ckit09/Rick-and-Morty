2de3918d844e36eb0a07099257ea215a97497358
ckit <a2036913@hotmail.com>
2023-03-25 22:57:08 +0800

    added character query function

--- src/pages/SingleContact.jsx
+++ src/pages/SingleContact.jsx
@@ -1,53 +1,69 @@
 import React from "react";
 import "./singleContact.css";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import axios from "axios";
 
 const SingleContact = () => {
   const inputRef = useRef();
   const [queryInput, setQueryInput] = useState("");
-  var searchResult = [];
+  const [searchResult, setSearchResult] = useState([]);
+
+  function handleEnter(e) {
+    if (e.key === "Enter") {
+      handleQuery();
+    }
+  }
+
+  useEffect(() => {
+    document
+      .querySelector(".contactContainer")
+      .addEventListener("keypress", handleEnter);
+    return () => window.removeEventListener("keypress", handleEnter);
+  }, []);
 
   function reset() {
-    inputRef.current.value = queryInput;
-    // console.log(document.querySelector(".contactGender").value);
+    inputRef.current.value = "";
   }
 
   function handleQuery() {
+    // reset
+    setSearchResult([])
+    document.querySelector(".errMsg").style.display = "none";
+    
     const query = {};
-    query.name = queryInput.toLowerCase();
-    query.status = document.querySelector(".contactStatus").value.toLowerCase()
-    query.gender = document.querySelector(".contactGender").value.toLowerCase()
-    reset()
+    let name = inputRef.current.value.toLowerCase();
+    let status = document.querySelector(".contactStatus").value.toLowerCase();
+    let gender = document.querySelector(".contactGender").value.toLowerCase();
+    query.name = `name=${name}`;
+    (status !== "false") ? (query.status = `&status=${status}`) : (query.status = "");
+    (gender !== "false") ? (query.gender = `&gender=${gender}`) : (query.gender = "");
     console.log(query);
-    // console.log(searchResult.push(fetch(`https://rickandmortyapi.com/api/character/?status=${query.status}&gender=${query.gender}`).json() ));
-    getCharacter(query)
-  }
 
-  const getCharacter = async ({query}) => {
-    var searchName
-    var searchGender
-    var searchStatus
-    if (query.name){searchName="name="}
-    if (query.status){searchStatus="&status="}
-    if (query.gender){searchGender="&gender="}
-    
-    const res = await fetch(`https://rickandmortyapi.com/api/character/?${searchName}${query.name}${searchStatus}${searchGender}${query.status}`);
-
-    if (!res.ok) {
-      throw new console.error(res.status);
+    if (!name) {
+      document.querySelector(".errMsg").innerText = "Please Enter Character Name";
+      document.querySelector(".errMsg").style.display = "block";
+      return;
     }
-    const result = await res.json();
-    
-    searchResult.push(result.results);
-    
-    console.log("result:", searchResult);
-  };
+
+    axios
+      .get(`https://rickandmortyapi.com/api/character/?${query.name}${query.gender}${query.status}`)
+      .then((result) => {
+        console.log(result.data);
+        setSearchResult(result.data.results);
+      })
+      .catch((e) => {
+        console.log(e.response.status);
+        document.querySelector(".errMsg").style.display = "block";
+        document.querySelector(".errMsg").innerText = "Character Not Found";
+      });
+  }
 
   return (
     <div className="singleContact">
       <div className="contactContainer">
-        <h1 className>Contact</h1>
+        <h1>Contact</h1>
         <input
+          autoFocus
           className="contactInput"
           type="text"
           placeholder="Type character name here ..."
@@ -59,23 +75,23 @@ const SingleContact = () => {
         />
         <div className="contactOption">
           <select className="contactStatus">
-            <option selected="selected" disabled hidden>
+            <option value="false" >
               Status
             </option>
             <option value="Alive">Alive</option>
             <option value="Dead">Dead</option>
             <option value="Unknown">Unknown</option>
-            <option value={false}>empty</option>
+            <option value="false"></option>
           </select>
           <select className="contactGender">
-            <option selected="selected" disabled hidden>
+            <option value="false">
               Gender
             </option>
             <option value="Male">Male</option>
             <option value="Female">Female</option>
             <option value="Genderless">Genderless</option>
             <option value="Unknown">Unknown</option>
-            <option value={false}>empty</option>
+            <option value="false"></option>
           </select>
         </div>
         <div className="contactButtons"></div>
@@ -87,7 +103,27 @@ const SingleContact = () => {
         </button>
 
         <div className="contactSearchResult">
-          {}
+          <span className="errMsg"></span>
+          <ul>
+            {
+              searchResult.length > 0 ?
+              <li className="singleContact-itemList">
+                <div className="singleContact-content" style={{fontSize: "20px"}}><u>Character Name</u></div>
+                <div className="singleContact-content" style={{fontSize: "20px"}}><u>Status</u></div>
+                <div className="singleContact-content" style={{fontSize: "20px"}}><u>Gender</u></div>
+                <div className="singleContact-content" style={{fontSize: "20px"}}><u>Species</u></div>
+              </li>
+              : null
+            }
+            {searchResult?.map((item, key) => (
+              <li key={key} className="singleContact-itemList">
+                <div className="singleContact-content">{item.name}</div>
+                <div className="singleContact-content">{item.status}</div>
+                <div className="singleContact-content">{item.gender}</div>
+                <div className="singleContact-content">{item.species}</div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
